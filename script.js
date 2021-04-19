691439a173f92a4d3fce85b7e3b9521e5654d2b0
Moringa School <moringa@moringa-thinkpad-x1-carbon-3rd-john-opana.mshome.net>
2021-04-19 16:41:09 +0300

    first commit

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-const submit=document.getElementById("submit");
+const submit = document.getElementById("submit");
 
 submit.addEventListener("click", function display() {
-    const day=document.getElementById("day").value;
-    const month=document.getElementById("month").value;
-    const year=document.getElementById("year").value;
-    const date=new Date(`${year}-${month}-${day}`);//new Date() to capture user date of birth
-    const dayOfBirth=date.getDay();
-    const male=document.getElementById("male");
-    const female=document.getElementById("female");
-    const display=document.getElementById("results");
-    const submit=document.getElementById("submit");
-    const days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    const maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-    const femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-    
-    if (day<=0 || day >31) {
-    display.innerHTML="Enter valid day!";
-    display.style.color="red";
-} else if (month<=0 || month>12) {
-        display.innerHTML="Enter valid moth!";
-        display.style.color="red";
+    const day = document.getElementById("day").value;
+    const month = document.getElementById("month").value;
+    const year = document.getElementById("year").value;
+    const date = new Date(`${year}-${month}-${day}`);//new Date() to capture user date of birth
+    const dayOfBirth = date.getDay();
+    const male = document.getElementById("male");
+    const female = document.getElementById("female");
+    const display = document.getElementById("results");
+    const submit = document.getElementById("submit");
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    if (day <= 0 || day > 31) {
+        display.innerHTML = "Enter valid day!";
+        display.style.color = "red";
+    } else if (month <= 0 || month > 12) {
+        display.innerHTML = "Enter valid moth!";
+        display.style.color = "red";
     } else if (male.checked) {
-    display.innerHTML =`Hello, you ware born on a ${days[dayOfBirth]} and your Akan name is ${maleNames[dayOfBirth]}`;
-    display.style.color="purple";
-    } else{
-    display.innerHTML =`Hello, you ware born on a ${days[dayOfBirth]} and your Akan name is ${femaleNames[dayOfBirth]}`;
-    display.style.color="purple";
-}
+        display.innerHTML = `Hello, you ware born on a ${days[dayOfBirth]} and your Akan name is ${maleNames[dayOfBirth]}`;
+        display.style.color = "wheat";
+    } else {
+        display.innerHTML = `Hello, you ware born on a ${days[dayOfBirth]} and your Akan name is ${femaleNames[dayOfBirth]}`;
+        display.style.color = "wheat";
+    }
 });
